31f8283390bedb55db8fa41d883faf9001f8bf47
Hernandez, Luis H <guicholh@gmail.com>
2024-03-30 16:57:54 -0500

    added cockroachdb connection

---
+++ cmd/web/helpers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+// send a generic 500 server error response to the user
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,36 +1,65 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"snippetbox.betocodes.io/internal/models"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+type application struct {
+	logger   *slog.Logger
+	snippets *models.SnippetModel
+}
 
 func main() {
 	// config flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", os.Getenv("DATABASE_URL"), "DB data source name")
 	flag.Parse()
 
 	// logger
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	// router or "mux"
-	mux := http.NewServeMux()
+	// connect to db
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
+	// dependency injection
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	logger.Info("Starting server", "addr", *addr)
 
-	logger.Info("Starting server", slog.Any("addr", *addr))
-	// logger.Info("Starting server", "addr", *addr)  OTHER OPTION
-	// logger.Info("Starting server", slog.String("addr", *addr))  OTHER OPTION
-
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
